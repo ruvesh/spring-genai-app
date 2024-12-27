4556fece47b1f4dc5abb28abf04bf3dd3a06d2d8
ruvesh <ruvesh2145@gmail.com>
2024-12-27 20:13:56 +0530

    made Book record and BookRequest package-private

--- src/main/java/io/github/ruvesh/spring_genai_app/prompts/recommendation/books/BookRequest.java
+++ src/main/java/io/github/ruvesh/spring_genai_app/prompts/recommendation/books/BookRequest.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class BookRequest {
+class BookRequest {
     private final UUID requestId = UUID.randomUUID();
     @Size(max = 30, message = "Author name is too long. Max allowed is 30 characters.")
     private String author;
